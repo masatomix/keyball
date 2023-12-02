77dbb62515bb3ae25a6b9c643fb8afc913ddff47
Masatomi KINO <masatomix@ki-no.org>
2023-12-03 00:48:33 +0900

    トラックボールを動かすと自動でマウスレイヤーに移動する設定を追加。 そのレイヤーにはマウスクリックなどが定義されているので、 トラックボールを触って「j」の押下で、自動で左クリックになるようにできた！

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/config.h
@@ -37,3 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define KEYBALL_CPI_DEFAULT 700 // マウス速度 (default: 700)
 #define KEYBALL_SCROLL_DIV_DEFAULT 6 // スクロール速度 (default: 6)
+
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
+#define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
