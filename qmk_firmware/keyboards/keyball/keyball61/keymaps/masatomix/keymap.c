2cdea351cace8a7b27846e8200de3d97d9e42067
Masatomi KINO <masatomix@ki-no.org>
2023-12-03 20:55:14 +0900

    Layer0を微調整

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , LT(1,KC_QUOT)  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LBRC  ,             KC_RBRC , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , SFT_T(KC_BSLS)  ,
-    _______  , KC_GRAVE , KC_LGUI, KC_LALT  ,LT(1,KC_LNG1),LT(2,KC_SPC),LT(3,KC_LNG2),       KC_BSPC , LT(2,KC_ENT), LT(1,KC_LNG2),KC_RGUI , _______ ,   KC_BSLS  , LT(3,KC_PSCR)
+    _______  , MO(4)    , KC_LGUI  , KC_LALT  , LT(1,KC_LNG1), LT(2,KC_SPC), LT(3,KC_LNG2),  KC_BSPC , LT(2,KC_ENT), LT(1,KC_LNG2),KC_RGUI , _______ ,MO(4) , LT(3,KC_GRAVE)
   ),
 
   [1] = LAYOUT_universal(
