6a4c450ef7bdb7011c5c870c2dfdc4384aa965a1
Masatomi KINO <masatomix@ki-no.org>
2023-12-09 13:46:06 +0900

    試行錯誤中
    レイヤーを5つにして、マウスレイヤーを2から3に変更した。
     スクロールレイヤーは最後の4
    
    キーマップは、44より外のキーは無効に。
    レイヤ1：Emacs風な矢印と、よく使うキー系を。日本語切替はココ。左親指ホーム
    レイヤ2：Functionキーと、まだやってないけど設定系を配置する用。左親指右
    レイヤ3：マウスレイヤーでマウス関連以外は配置しない。
    レイヤ4：スクロールレイヤーで、あとは記号系。左親指左。

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE // 有効化
-#define AUTO_MOUSE_DEFAULT_LAYER 2 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_DEFAULT_LAYER 3 // 切り替えるマウスレイヤー番号を指定
 #define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
 
 
@@ -49,6 +49,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRECISION_ENABLE // 有効化
 #define PRECISION_CPI 3  // 下げた時のCPI (1/100の値を指定。左記ならCPI 400)
 
-// #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
 #define KEYBALL_SCROLLSNAP_ENABLE 0
