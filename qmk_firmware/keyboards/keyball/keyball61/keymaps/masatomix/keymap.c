ed8c88a228a9aaed6380ce859a75695c96e28912
Masatomi KINO <masatomix@ki-no.org>
2023-12-09 22:31:14 +0900

    試行錯誤中

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/masatomix/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F11   , LCTL(KC_UP ), LCTL(KC_DOWN) , KC_PGUP  , KC_UP   , PRC_SW  ,                          _______ , _______  , KC_PSCR   , KC_PGUP  , KC_UP     , KC_F12  ,
    S(KC_LCTL), KC_VOLD    , KC_VOLU       , KC_DEL   , KC_RGHT , LGUI(KC_ESC)       ,                KC_BSPC , KC_DOWN  , KC_UP     , KC_RGHT  , _______   , _______ ,
     _______  , KC_BTN4    , KC_BTN5       , KC_PGDN  , KC_DOWN , KC_LEFT , _______  ,     _______  , KC_DOWN , KC_LEFT  , _______   , KC_PGDN  , _______   , _______ ,
-    _______  , _______    , S(KC_LGUI)    , S(KC_LALT), _______, _______ , _______  ,     KC_LNG1   , KC_LNG2 , _______  , S(KC_RGUI), _______  , S(KC_RALT), _______
+    _______  , _______    , S(KC_LGUI)    , S(KC_LALT), _______, _______ , _______  ,      KC_DEL ,  _______  , _______  , S(KC_RGUI), _______  , S(KC_RALT), _______
   ),
 
   [2] = LAYOUT_universal(
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , S(KC_1)  , S(KC_2)  , S(KC_3)  , S(KC_4)  , S(KC_5)  ,                                     S(KC_6)   , S(KC_7) , S(KC_8)  , S(KC_9)  , S(KC_0)  , S(KC_MINS),
     KC_LCTL  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5 ,                                          KC_6     ,   KC_7  , KC_8     , KC_9     , KC_0     , KC_MINS,
     _______  , _______  , _______  , _______  , KC_LBRC  , S(KC_LBRC)  , _______  ,            _______ ,  S(KC_RBRC),KC_RBRC  , _______  , _______  , _______  , _______ ,
-    _______  , _______  , _______  , _______  , _______  , _______     , _______  ,            KC_DEL ,  _______   , _______ , _______  , _______  , _______  , _______
+    _______  , _______  , _______  , _______  , _______  , _______     , _______  ,           KC_LNG1   , KC_LNG2  , _______ , _______  , _______  , _______  , _______
   ),
 
         //   [3] = LAYOUT_universal(
